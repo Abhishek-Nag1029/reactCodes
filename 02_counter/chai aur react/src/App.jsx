a4a495fdb3cd8346990d1e0d5386b4c66c4159c5
Abhishek-Nag1029 <abhisheknic18@gmail.com>
2024-05-24 19:27:59 +0530

    first commit

---
+++ 02_counter/chai aur react/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter=3
+
+  let [counter,setCounter]=useState(15);
+
+  const addValue=()=>{
+    // console.log("clicked",counter);
+    if(counter+1<=20)
+    counter=counter +1 ;
+    setCounter(counter);
+  }
+
+  const removeValue=()=>{
+    if(counter-1>=0)
+    counter=counter - 1 ;
+    setCounter(counter);
+  }
+
+  return (
+    <>
+    <h1>chai aur React</h1>
+    <h2>counter value :{counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Increase Value {counter}</button>
+    <br/>
+    <button
+    onClick={removeValue}
+    >Decrease Value {counter}</button>
+    <p>{counter}</p>
+
+    </>
+  )
+}
+
+export default App
